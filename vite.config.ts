4b7ac81be8f67d754bca1a4a3661c85a2b1a0c31
Daniel Smith <dan@filemaker.consulting>
2024-04-25 14:32:15 -0700

    minimize html
    `npm i vite-plugin-html -D`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte(), viteSingleFile()],
+	plugins: [
+		svelte(),
+		viteSingleFile(),
+		createHtmlPlugin({
+			minify: true,
+		}),
+	],
 });
