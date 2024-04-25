6067d00e45f79df09882834b81226eec5541d20f
Daniel Smith <dan@filemaker.consulting>
2024-04-25 14:17:41 -0700

    install and configure prettier
    `npm i -D prettier prettier-plugin-svelte`

--- src/App.svelte
+++ src/App.svelte
@@ -1,72 +1,77 @@
 <script lang="ts">
-  import { onMount } from 'svelte'
-  import { mockGoferScript } from 'fm-mock'
-	import FMGofer from 'fm-gofer'
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	import { onMount } from "svelte";
+	import { mockGoferScript } from "fm-mock";
+	import FMGofer from "fm-gofer";
+	import svelteLogo from "./assets/svelte.svg";
+	import viteLogo from "./assets/vite.svg";
+	import Counter from "./lib/Counter.svelte";
 
-  let loading = true
-  let fmData = ''
+	let loading = true;
+	let fmData = "";
 
-  if (import.meta.env.DEV) {
-    mockGoferScript('WV FileMaker-SvelteWebViewer', {
-      resultFromFM: 'This text could come from a FileMakser script, but fm-mock lets you develop FileMaker webviewer apps in the browser; even if they call FileMaker scripts! So this text is a "mock" of an actual scripts response. Note that the slow "loading" time is also defined by the mocked script result, which allows you to replicate real-life scenarios where a script might take some time to return a result.',
-      logParams: true,
-      delay: 3000
-    })
-  }
+	if (import.meta.env.DEV) {
+		mockGoferScript("WV FileMaker-SvelteWebViewer", {
+			resultFromFM:
+				'This text could come from a FileMakser script, but fm-mock lets you develop FileMaker webviewer apps in the browser; even if they call FileMaker scripts! So this text is a "mock" of an actual scripts response. Note that the slow "loading" time is also defined by the mocked script result, which allows you to replicate real-life scenarios where a script might take some time to return a result.',
+			logParams: true,
+			delay: 3000,
+		});
+	}
 
 	onMount(async () => {
-		const res = await FMGofer.PerformScript('WV FileMaker-SvelteWebViewer', 'parameter from the webviewer')
-		loading = false
+		const res = await FMGofer.PerformScript(
+			"WV FileMaker-SvelteWebViewer",
+			"parameter from the webviewer",
+		);
+		loading = false;
 		if (import.meta.env.DEV) {
-			console.log('FileMaker script response:', res)
+			console.log("FileMaker script response:", res);
 		}
-    fmData = res
-	})
+		fmData = res;
+	});
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+	<div>
+		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+			<img src={viteLogo} class="logo" alt="Vite Logo" />
+		</a>
 
-  <div class="card">
-    <Counter />
-  </div>
+		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
+			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+		</a>
+	</div>
+	<h1>Vite + Svelte</h1>
 
-  {#if fmData}
-    <p class="fmData">{fmData}</p>
-  {:else}
-    <p>loading...</p>
-  {/if}
+	<div class="card">
+		<Counter />
+	</div>
+
+	{#if fmData}
+		<p class="fmData">{fmData}</p>
+	{:else}
+		<p>loading...</p>
+	{/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .fmData {
-    border-radius: 4px;
-    border: 1px solid #cfcfcf;
-    padding: 0.6em 1.2em;
-    background-color: #f9f9f9;
-    font-family: monospace;
-  }
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+		transition: filter 300ms;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	.logo.svelte:hover {
+		filter: drop-shadow(0 0 2em #ff3e00aa);
+	}
+	.fmData {
+		border-radius: 4px;
+		border: 1px solid #cfcfcf;
+		padding: 0.6em 1.2em;
+		background-color: #f9f9f9;
+		font-family: monospace;
+	}
 </style>
